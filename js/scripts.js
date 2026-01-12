728a935b1d4c9534d12c9357a5ace44a1252406e
Antonioo <240267@utxicotepec.edu.mx>
2026-01-12 15:14:00 -0600

    Practica 02
    add: Se agrego el archivo del ejercicio 01 - Declaracion de Variables, aun sin terminar

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// 1. declaracion de variables - VAR
+
+var miNombre = "Antonio";
+var misApellidos;
+
+// funcion console.log para imprimir valor en la variable
+console.log("El valor de la varibale miNombre es:", miNombre)
+console.log("El valor de la varibale misApellidos es:", misApellidos)
+
+// en este momento la variable ya tiene valor
+misApellidos = "Ricaño Reyes"
+console.log("El valor de la varibale misApellidos es:", misApellidos)
+
+// e
+miNombre = "José Antonio";
+console.log("Hola", miNombre, misApellidos)
